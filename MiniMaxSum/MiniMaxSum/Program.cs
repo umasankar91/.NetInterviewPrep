0b38d9938253724b323d12022f9e9dc5a40ba61f
umaSankar <umasankar.perugupalli@gmail.com>
2025-05-28 23:31:53 +0530

    Pushing Minimax Sum problem

---
+++ MiniMaxSum/MiniMaxSum/Program.cs
@@ -0,0 +1,23 @@
+﻿class Result
+{
+    public static void miniMaxSum(List<int> arr)
+    {
+        long totalSum = arr.Sum(x => (long)x);
+        long min = totalSum - arr.Max();
+        long max = totalSum - arr.Min();
+
+        Console.WriteLine(string.Format("{0} {1}", min, max));
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
